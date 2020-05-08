75d8e5288a26799931bac0387c2f29d00dd7f79a
edward <edward@gmail.com>
2020-05-08 15:35:36 +0800

    完成新增發票紀錄及建立發票資料表

---
+++ save_invoice.php
@@ -0,0 +1,29 @@
+<?php
+include "./com/base.php";
+
+
+$sql="insert into invoice (
+    `period`,
+    `year`,
+    `code`,
+    `number`,
+    `expend`) values (
+        '".$_POST['period']."',
+        '".$_POST['year']."',
+        '".$_POST['code']."',
+        '".$_POST['number']."',
+        '".$_POST['expend']."')";
+
+echo  $sql;
+$res=$pdo->exec($sql);
+
+if($res==1){
+    echo "新增成功";
+}
+else{
+    echo "新增失敗";
+}
+
+
+
+?>
